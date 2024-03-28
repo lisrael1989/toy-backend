b2ced785acc870b55297c1dfc622edbdefc86871
israel <lisrael1989@gmail.com>
2024-03-28 20:24:19 +0200

    adding changes from frontend

---
+++ api/user/user.routes.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+import { requireAuth, requireAdmin } from "../../middlewares/requireAuth.middleware.js"
+import { getUser, getUsers, deleteUser, updateUser } from "./user.controller.js"
+
+export const userRoutes = express.Router()
+
+userRoutes.get("/", getUsers)
+userRoutes.get("/:id", getUser)
+userRoutes.put("/:id", updateUser)
+
+userRoutes.delete("/:id", requireAuth, requireAdmin, deleteUser)
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 import express from "express"
-import cors from "cors"
 import cookieParser from "cookie-parser"
-import { toyService } from "./services/toy.service.js"
+import cors from "cors"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+// import { toyService } from "./services/toy.service.js"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+import { logger } from "./services/logger.service.js"
+logger.info("server.js loaded...")
 
 const app = express()
 
 // Express App Config
-app.use(express.json())
 app.use(cookieParser())
+app.use(express.json())
 app.use(express.static("public"))
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"))
+  app.use(express.static(path.resolve(__dirname, "public")))
+  console.log("__dirname: ", __dirname)
 } else {
+  // Configuring CORS
   const corsOptions = {
     origin: [
       "http://127.0.0.1:4040",
@@ -25,73 +34,86 @@ if (process.env.NODE_ENV === "production") {
   app.use(cors(corsOptions))
 }
 
-app.get("/api/toy", (req, res) => {
-  const { filterBy = {}, sort = {} } = req.query.params
+import { authRoutes } from "./api/auth/auth.routes.js"
+import { userRoutes } from "./api/user/user.routes.js"
+import { toyRoutes } from "./api/toy/toy.routes.js"
 
-  toyService
-    .query(filterBy, sort)
-    .then((toys) => {
-      res.send(toys)
-    })
-    .catch((err) => {
-      console.log("Had issues getting toys", err)
-      res.status(400).send({ msg: "Had issues getting toys" })
-    })
+// routes
+app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
+app.use("/api/toy", toyRoutes)
+
+app.get("/**", (req, res) => {
+  res.sendFile(path.resolve("public/index.html"))
 })
 
-app.get("/api/toy/:id", (req, res) => {
-  const toyId = req.params.id
-  toyService
-    .getById(toyId)
-    .then((toy) => {
-      res.send(toy)
-    })
-    .catch((err) => {
-      console.log("Had issues getting toy", err)
-      res.status(400).send({ msg: "Had issues getting toy" })
-    })
-})
+// app.get("/api/toy", (req, res) => {
+//   const { filterBy = {}, sort = {} } = req.query.params
 
-app.delete("/api/toy/:id", (req, res) => {
-  const toyId = req.params.id
-  toyService
-    .remove(toyId)
-    .then(() => {
-      res.end("Done!")
-    })
-    .catch((err) => {
-      console.log("Had issues deleting toy", err)
-      res.status(400).send({ msg: "Had issues deleteing toy" })
-    })
-})
+//   toyService
+//     .query(filterBy, sort)
+//     .then((toys) => {
+//       res.send(toys)
+//     })
+//     .catch((err) => {
+//       console.log("Had issues getting toys", err)
+//       res.status(400).send({ msg: "Had issues getting toys" })
+//     })
+// })
 
-app.post("/api/toy", (req, res) => {
-  const toy = req.body
-  toyService
-    .save(toy)
-    .then((savedToy) => {
-      res.send(savedToy)
-    })
-    .catch((err) => {
-      console.log("Had issues adding toy", err)
-      res.status(400).send({ msg: "Had issues adding toy" })
-    })
-})
+// app.get("/api/toy/:id", (req, res) => {
+//   const toyId = req.params.id
+//   toyService
+//     .getById(toyId)
+//     .then((toy) => {
+//       res.send(toy)
+//     })
+//     .catch((err) => {
+//       console.log("Had issues getting toy", err)
+//       res.status(400).send({ msg: "Had issues getting toy" })
+//     })
+// })
 
-app.put("/api/toy/:id", (req, res) => {
-  const toy = req.body
-  toyService
-    .save(toy)
-    .then((savedToy) => {
-      res.send(savedToy)
-    })
-    .catch((err) => {
-      console.log("Had issues updating toy", err)
-      res.status(400).send({ msg: "Had issues updating toy" })
-    })
-})
+// app.delete("/api/toy/:id", (req, res) => {
+//   const toyId = req.params.id
+//   toyService
+//     .remove(toyId)
+//     .then(() => {
+//       res.end("Done!")
+//     })
+//     .catch((err) => {
+//       console.log("Had issues deleting toy", err)
+//       res.status(400).send({ msg: "Had issues deleteing toy" })
+//     })
+// })
 
-const port = 4040
+// app.post("/api/toy", (req, res) => {
+//   const toy = req.body
+//   toyService
+//     .save(toy)
+//     .then((savedToy) => {
+//       res.send(savedToy)
+//     })
+//     .catch((err) => {
+//       console.log("Had issues adding toy", err)
+//       res.status(400).send({ msg: "Had issues adding toy" })
+//     })
+// })
+
+// app.put("/api/toy/:id", (req, res) => {
+//   const toy = req.body
+//   toyService
+//     .save(toy)
+//     .then((savedToy) => {
+//       res.send(savedToy)
+//     })
+//     .catch((err) => {
+//       console.log("Had issues updating toy", err)
+//       res.status(400).send({ msg: "Had issues updating toy" })
+//     })
+// })
+
+const port = process.env.PORT || 4040
 app.listen(port, () => {
-  console.log("Server is up and listening to", port)
+  logger.info("Server is running on port:" + port)
 })
